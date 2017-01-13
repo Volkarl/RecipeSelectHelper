4202b78a574bef2703467a48df0f902d6e810943
Karlzone <Karlzone0@gmail.com>
2017-01-13 22:48:52 +0100

    Grids partly setup and the listView columns now scale properly by size.

--- RecipeSelectHelper/MainWindow.xaml.cs
+++ RecipeSelectHelper/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,22 @@ namespace RecipeSelectHelper
         public MainWindow()
         {
             InitializeComponent();
+            Show();
+            ListView_SizeChanged(ListView_Recipes, null);
+
+        }
+
+        private void ListView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ListView listView = sender as ListView;
+            GridView gridView = listView.View as GridView;
+            var remainingWidth = listView.ActualWidth - SystemParameters.VerticalScrollBarWidth;
+
+            for (Int32 i = 1; i < gridView.Columns.Count; i++)
+            {
+                remainingWidth -= gridView.Columns[i].ActualWidth;
+            }
+            gridView.Columns[0].Width = remainingWidth;
         }
     }
 }
