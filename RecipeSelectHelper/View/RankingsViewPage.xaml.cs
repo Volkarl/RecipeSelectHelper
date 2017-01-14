63e94ef45861a308c60bdecdaadae4664581c506
Karlzone <Karlzone0@gmail.com>
2017-01-14 16:21:01 +0100

    stuff

---
+++ RecipeSelectHelper/View/RankingsViewPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RecipeSelectHelper.View
+{
+    /// <summary>
+    /// Interaction logic for RankingsViewPage.xaml
+    /// </summary>
+    public partial class RankingsViewPage : Page
+    {
+        public RankingsViewPage()
+        {
+            InitializeComponent();
+            this.Loaded += RankingsViewPageLoaded;
+        }
+
+        private void RankingsViewPageLoaded(object sender, RoutedEventArgs e)
+        {
+            ListView_SizeChanged(ListView_Recipes, null);
+        }
+
+        private void ListView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ListView listView = sender as ListView;
+            GridView gridView = listView.View as GridView;
+            var remainingWidth = listView.ActualWidth - 5;
+
+            for (Int32 i = 1; i < gridView.Columns.Count; i++)
+            {
+                remainingWidth -= gridView.Columns[i].ActualWidth;
+            }
+            gridView.Columns[0].Width = remainingWidth;
+        }
+    }
+}
