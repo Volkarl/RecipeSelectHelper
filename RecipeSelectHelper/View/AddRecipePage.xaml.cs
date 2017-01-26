abf70e189c1725080ccbe3d2eb24c69e559b872e
Volkarl <karlzone0@gmail.com>
2017-01-26 16:15:59 +0100

    Adding amount now almost works, albeit not pretty.

--- RecipeSelectHelper/View/AddRecipePage.xaml.cs
+++ RecipeSelectHelper/View/AddRecipePage.xaml.cs
@@ -46,11 +46,53 @@ namespace RecipeSelectHelper.View
 
             foreach (Product ingredients in _parent.Data.AllProducts)
             {
+                var stackPanel = new StackPanel();
                 var checkBox = new CheckBox();
+                var checkedContent = new StackPanel();
+                var label1 = new Label();
+                var textBox = new TextBox();
+                var label2 = new Label();
+
                 checkBox.Content = ingredients.Name;
                 checkBox.Margin = new Thickness(4);
-                ItemsControl_Ingredients.Items.Add(checkBox);
+
+                label1.Content = "Amount: ";
+                textBox.Width = 40;
+                label2.Content = " kg.";
+
+                checkedContent.Orientation = Orientation.Horizontal;
+                checkedContent.Children.Add(label1);
+                checkedContent.Children.Add(textBox);
+                checkedContent.Children.Add(label2);
+                checkedContent.Visibility = Visibility.Collapsed;
+
+                stackPanel.Orientation = Orientation.Horizontal;
+                stackPanel.Children.Add(checkBox);
+                stackPanel.Children.Add(checkedContent);
+
+                checkBox.Checked += IngredientSelected;
+                checkBox.Unchecked += IngredientSelected;
+
+                ItemsControl_Ingredients.Items.Add(stackPanel);
             }
+        }
+
+        private void IngredientSelected(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+            StackPanel stackPanel = checkBox.Parent as StackPanel;
+            StackPanel checkedContent = stackPanel.Children[1] as StackPanel;
+
+            if (checkBox.IsChecked.Value)
+            {
+                checkedContent.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                checkedContent.Visibility = Visibility.Collapsed;
+            }
+
+            // Figure out how to do it horizontally? Its not pretty as it looks right now.
         }
 
         private void Button_Back_Click(object sender, RoutedEventArgs e)
@@ -66,26 +108,8 @@ namespace RecipeSelectHelper.View
             string name = TextBox_RecipeName.Text;
             string description = TextBox_RecipeDescription.Text;
             string instruction = TextBox_RecipeInstruction.Text;
-            var categories = new List<RecipeCategory>();
-
-            var indexList = new List<int>();
-            for (int i = 0; i < ItemsControl_Categories.Items.Count; i++)
-            {
-                if (((CheckBox)(ItemsControl_Categories.Items[i])).IsChecked.Value)
-                {
-                    indexList.Add(i);
-                }
-
-            }
-            foreach (int index in indexList)
-            {
-                categories.Add(_parent.Data.AllRecipeCategories[index]);
-            }
-
-            var ingredients = new List<Ingredient>();
-
-            // CLEAN THIS UP AND ADD ONE FOR INGREDIENTS
-
+            List<RecipeCategory> categories = GetCheckedCategories();
+            List<Ingredient> ingredients = GetCheckedIngredients();
 
             try
             {
@@ -101,11 +125,69 @@ namespace RecipeSelectHelper.View
             // ADD ERROR HANDLING IN RECIPE AND OTHER (Name != null) (Need to be unique)
         }
 
+        private List<Ingredient> GetCheckedIngredients()
+        {
+            var ingredients = new List<Ingredient>();
+            var checkedProducts = new List<Product>();
+            var indexList = new List<int>();
+
+
+
+            // THIS PROBABLY NO LONGER WORKS!!!! BECAUSE CHECKBOX IS ELEMENT 0 INSIDE ONE STACKPANEL
+
+            for (int i = 0; i < ItemsControl_Ingredients.Items.Count; i++)
+            {
+                if (((CheckBox)(ItemsControl_Ingredients.Items[i])).IsChecked.Value)
+                {
+                    indexList.Add(i);
+                }
+            }
+            foreach (int index in indexList)
+            {
+                checkedProducts.Add(_parent.Data.AllProducts[index]);
+            }
+
+            foreach (Product product in checkedProducts)
+            {
+                ingredients.Add(new Ingredient(0, product)); // HERE IT NEEDS TO TAKE THE CORRECT VALUE
+            }
+            return ingredients;
+        }
+
+        private List<RecipeCategory> GetCheckedCategories()
+        {
+            var categories = new List<RecipeCategory>();
+            var indexList = new List<int>();
+
+            for (int i = 0; i < ItemsControl_Categories.Items.Count; i++)
+            {
+                if (((CheckBox)(ItemsControl_Categories.Items[i])).IsChecked.Value)
+                {
+                    indexList.Add(i);
+                }
+            }
+            foreach (int index in indexList)
+            {
+                categories.Add(_parent.Data.AllRecipeCategories[index]);
+            }
+            return categories;
+        }
+
         private void ClearUIElements()
         {
             TextBox_RecipeName.Text = string.Empty;
             TextBox_RecipeDescription.Text = string.Empty;
             TextBox_RecipeInstruction.Text = string.Empty;
+        }
+
+        private void Button_AddCategory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_AddIngredient_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
