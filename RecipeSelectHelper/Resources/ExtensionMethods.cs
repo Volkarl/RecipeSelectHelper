f07d9c2cdd1e82afe976bd12b5898f81e4a56ac5
Volkarl <karlzone0@gmail.com>
2017-02-09 01:42:18 +0100

    ComboBox in SortingMethodsPage works (partially done)

---
+++ RecipeSelectHelper/Resources/ExtensionMethods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeSelectHelper.Resources
+{
+    public static class ExtensionMethods
+    {
+        public static string GetDescription(this Enum value)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            if (name != null)
+            {
+                FieldInfo field = type.GetField(name);
+                if (field != null)
+                {
+                    DescriptionAttribute attr =
+                           Attribute.GetCustomAttribute(field,
+                             typeof(DescriptionAttribute)) as DescriptionAttribute;
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static T GetEnumValueFromDescription<T>(string description)
+        {
+            MemberInfo[] fis = typeof(T).GetFields();
+
+            foreach (var fi in fis)
+            {
+                DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attributes != null && attributes.Length > 0 && attributes[0].Description == description)
+                    return (T)Enum.Parse(typeof(T), fi.Name);
+            }
+
+            throw new KeyNotFoundException("Enum Value not found.");
+            // return (T)Enum.Parse(typeof(T), description);
+        }
+    }
+}
--- RecipeSelectHelper/View/SortingMethodsPage.xaml.cs
+++ RecipeSelectHelper/View/SortingMethodsPage.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using RecipeSelectHelper.Resources;
 
 namespace RecipeSelectHelper.View
 {
@@ -31,14 +22,133 @@ namespace RecipeSelectHelper.View
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-
         public SortingMethodsPage(MainWindow parent)
         {
-            this._parent = parent;
+            _parent = parent;
             DataContext = this;
-            this.Loaded += SortingMethodsPage_Loaded;
+            LoadObservableObjects();
+            Loaded += SortingMethodsPage_Loaded;
             InitializeComponent();
         }
+
+        private void LoadObservableObjects()
+        {
+            LoadPref1Settings();
+            Pref2Settings = new ObservableCollection<string>();
+            Pref3Settings = new ObservableCollection<string>();
+            Pref1Value = String.Empty;
+            Pref2Value = String.Empty;
+            Pref3Value = String.Empty;
+        }
+
+        #region ObservableObjects
+
+        private ObservableCollection<string> _pref1Settings;
+        public ObservableCollection<string> Pref1Settings
+        {
+            get { return _pref1Settings; }
+            set { _pref1Settings = value; OnPropertyChanged(nameof(Pref1Settings)); }
+        }
+
+        private string _pref1Value;
+        public string Pref1Value
+        {
+            get { return _pref1Value; }
+            set { _pref1Value = value; OnPropertyChanged(nameof(Pref1Value)); }
+        }
+
+        private ObservableCollection<string> _pref2Settings;
+        public ObservableCollection<string> Pref2Settings
+        {
+            get { return _pref2Settings; }
+            set { _pref2Settings = value; OnPropertyChanged(nameof(Pref2Settings)); }
+        }
+
+        private string _pref2Value;
+        public string Pref2Value
+        {
+            get { return _pref2Value; }
+            set { _pref2Value = value; OnPropertyChanged(nameof(Pref2Value)); }
+        }
+
+        private ObservableCollection<string> _pref3Settings;
+        public ObservableCollection<string> Pref3Settings
+        {
+            get { return _pref3Settings; }
+            set { _pref3Settings = value; OnPropertyChanged(nameof(Pref3Settings)); }
+        }
+
+        private string _pref3Value;
+        public string Pref3Value
+        {
+            get { return _pref3Value; }
+            set { _pref3Value = value; OnPropertyChanged(nameof(Pref3Value)); }
+        }
+
+        #endregion
+
+        #region PreferenceChoices
+
+        private enum Pref1Choices
+        {
+            [Description("Product Category")]
+            ByProductCategory,
+            [Description("Recipe Category")]
+            ByRecipeCategory,
+            [Description("Specific Ingredients")]
+            BySpecificIngredients,
+            [Description("Ingredients Owned")]
+            ByIngredientsOwned,
+            [Description("Uses Expiring Food First")]
+            ByExpirationDate,
+        }
+
+        private Pref1Choices _pref1Choice;
+        private Pref1Choices Pref1Choice
+        {
+            get { return _pref1Choice; }
+            set { _pref1Choice = value;
+                LoadPref2Settings(value);
+            }
+        }
+        // Missing two here and so and so forth
+        #endregion
+
+        #region LoadPreferenceChoices
+
+        private void LoadPref1Settings()
+        {
+            var s = new List<string>
+            {
+                Pref1Choices.ByProductCategory.GetDescription(),
+                Pref1Choices.ByRecipeCategory.GetDescription(),
+                Pref1Choices.BySpecificIngredients.GetDescription(),
+                Pref1Choices.ByIngredientsOwned.GetDescription(),
+                Pref1Choices.ByExpirationDate.GetDescription()
+            };
+            Pref1Settings = new ObservableCollection<string>(s);
+        }
+
+        private void LoadPref2Settings(Pref1Choices value)
+        {
+            switch (value)
+            {
+                case Pref1Choices.ByProductCategory:
+                    break;
+                case Pref1Choices.ByRecipeCategory:
+                    break;
+                case Pref1Choices.BySpecificIngredients:
+                    break;
+                case Pref1Choices.ByIngredientsOwned:
+                    break;
+                case Pref1Choices.ByExpirationDate:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, null);
+            }
+        }
+
+        #endregion
 
         private void SortingMethodsPage_Loaded(object sender, RoutedEventArgs e)
         {
@@ -51,6 +161,21 @@ namespace RecipeSelectHelper.View
         private void Button_FinalizePreference_Click(object sender, RoutedEventArgs e)
         {
 //            StackPanel_SelectedPreferences.Children.Add(sortingMethod);
+        }
+
+        private void ComboBox_Pref1_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _pref1Choice = ExtensionMethods.GetEnumValueFromDescription<Pref1Choices>(Pref1Value);
+        }
+
+        private void ComboBox_Pref2_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ComboBox_Pref3_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
