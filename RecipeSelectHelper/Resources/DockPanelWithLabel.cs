6cd326efbec34d1415dcc751987108f5eaeaa9f4
Volkarl <karlzone0@gmail.com>
2017-02-11 17:24:02 +0100

    Preferences pretty much done?

---
+++ RecipeSelectHelper/Resources/DockPanelWithLabel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RecipeSelectHelper.Resources
+{
+    public class DockPanelWithLabel : DockPanel
+    {
+        public UIElement SecondElement { get; private set; }
+        public DockPanelWithLabel(object labelContents, UIElement secondElement)
+        {
+            SecondElement = secondElement;
+            Children.Add(new Label {Content = labelContents, Width = 200});
+            Children.Add(secondElement);
+        }
+    }
+}
--- RecipeSelectHelper/View/SortingMethodsPage.xaml.cs
+++ RecipeSelectHelper/View/SortingMethodsPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -6,9 +7,16 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Forms;
 using RecipeSelectHelper.Model;
 using RecipeSelectHelper.Model.SortingMethods;
 using RecipeSelectHelper.Resources;
+using Button = System.Windows.Controls.Button;
+using ComboBox = System.Windows.Controls.ComboBox;
+using Label = System.Windows.Controls.Label;
+using MessageBox = System.Windows.MessageBox;
+using Orientation = System.Windows.Controls.Orientation;
 
 namespace RecipeSelectHelper.View
 {
@@ -83,7 +91,7 @@ namespace RecipeSelectHelper.View
         {
             get { return _pref1Choice; }
             set { _pref1Choice = value;
-                UniformGrid_SelectedPreference.Children.RemoveRange(1, 2);
+                StackPanel_CurrentPreference.Children.RemoveRange(1, 2);
                 LoadPref2Settings(value);
             }
         }
@@ -107,41 +115,77 @@ namespace RecipeSelectHelper.View
 
         private void LoadPref2Settings(PreferenceTopic value)
         {
-            var ui1 = new UIElement();
-            var ui2 = new UIElement();
+            UIElement ui1 = null;
+            UIElement ui2 = null;
             switch (value)
             {
                 case PreferenceTopic.ByProductCategory:
-                    ui1 = CreateComboBox(_parent.Data.AllProductCategories.ConvertAll(x => x.Name));
-                    ui2 = new IntegerTextBox();
+                    ui1 = new DockPanelWithLabel("If product is: ",
+                        CreateComboBoxToDisplayName(_parent.Data.AllProductCategories));
+                    ui2 = new DockPanelWithLabel("Then add value: ", 
+                        new IntegerTextBox());
                     break;
                 case PreferenceTopic.ByRecipeCategory:
-                    ui1 = CreateComboBox(_parent.Data.AllRecipeCategories.ConvertAll(x => x.Name));
-                    ui2 = new IntegerTextBox();
+                    ui1 = new DockPanelWithLabel("If recipe is: ",
+                        CreateComboBoxToDisplayName(_parent.Data.AllRecipeCategories));
+                    ui2 = new DockPanelWithLabel("Then add value: ", 
+                        new IntegerTextBox());
                     break;
                 case PreferenceTopic.BySpecificIngredients:
-                    ui1 = CreateComboBox(_parent.Data.AllProducts.ConvertAll(x => x.Name));
-                    ui2 = new IntegerTextBox();
+                    ui1 = new DockPanelWithLabel("If recipe contains ingredient: ", 
+                        CreateComboBoxToDisplayName(_parent.Data.AllProducts));
+                    ui2 = new DockPanelWithLabel("Then add value: ", 
+                        new IntegerTextBox());
                     break;
                 case PreferenceTopic.ByIngredientsOwned:
-                    ui1 = new IntegerTextBox();
+                    ui1 = new Label {Content = "If recipe contains any ingredient from fridge"};
+                    ui2 = new DockPanelWithLabel("Then add value: ", 
+                        new IntegerTextBox());
                     break;
                 case PreferenceTopic.ByExpirationDate:
-                    ui1 = new IntegerTextBox();
+                    ui1 = new Label {Content = "LOTS OF WEIRD STUFF AND CONFIGURATIONS SHOULD BE HERE!"};
+                    ui2 = new IntegerTextBox();
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(value), value, null);
             }
 
-            UniformGrid_SelectedPreference.Children.Add(ui1);
-            UniformGrid_SelectedPreference.Children.Add(ui2);
+            if (ui1 != null)
+            {
+                StackPanel_CurrentPreference.Children.Add(ui1);
+            }
+            if (ui2 != null)
+            {
+                StackPanel_CurrentPreference.Children.Add(ui2);
+            }
         }
 
-        private ComboBox CreateComboBox(IEnumerable<String> items)
+        private DockPanel CreateDockPanelWithLabel(object labelContent, UIElement secondElement)
+        {
+            var dockPanel = new DockPanel();
+            dockPanel.Children.Add(new Label {Content = labelContent, Width = 200});
+            dockPanel.Children.Add(secondElement);
+            return dockPanel;
+        }
+
+        private UniformGrid CreateUniformGridWithLabel(object labelContent, UIElement secondElement)
+        {
+            var uniformGrid = new UniformGrid
+            {
+                Rows = 1,
+                Columns = 2
+            };
+            uniformGrid.Children.Add(new Label {Content = labelContent});
+            uniformGrid.Children.Add(secondElement);
+            return uniformGrid;
+        }
+
+        private ComboBox CreateComboBoxToDisplayName<T>(IEnumerable<T> items)
         {
             var cbx = new ComboBox
             {
-                ItemsSource = new ObservableCollection<string>(items)
+                ItemsSource = new ObservableCollection<T>(items),
+                DisplayMemberPath = "Name"
             };
             return cbx;
         }
@@ -178,49 +222,112 @@ namespace RecipeSelectHelper.View
             switch (choice)
             {
                 case PreferenceTopic.ByProductCategory:
-                    preferenceMethod = SortByProductCategory();
+                {
+                    ProductCategory pc;
+                    int value;
+                    if (GetValuesFromUI(out pc, out value, StackPanel_CurrentPreference))
+                    {
+                        preferenceMethod = SortByProductCategory(pc, value);
+                    }
+                    else
+                    {
+                        throw new NullReferenceException("Invalid input");
+                    }
+                        //var dp = StackPanel_CurrentPreference.Children[1] as DockPanelWithLabel;
+                        //var cbx = dp.SecondElement as ComboBox;
+                        //var pc = cbx.SelectedValue as ProductCategory;
+                        //var itxt = StackPanel_CurrentPreference.Children[2] as IntegerTextBox;
+                        //int value;
+                        //if (int.TryParse(itxt.Text, out value))
+                        //{
+                        //    preferenceMethod = SortByProductCategory(pc, value);
+                        //}
+                        //else
+                        //{
+                        //    throw new NullReferenceException("Invalid input");
+                        //}
                     break;
+                }
                 case PreferenceTopic.ByRecipeCategory:
-                    preferenceMethod = SortByRecipeCategory();
+                {
+                    RecipeCategory rc;
+                    int value;
+                    if (GetValuesFromUI(out rc, out value, StackPanel_CurrentPreference))
+                    {
+                        preferenceMethod = SortByRecipeCategory(rc, value);
+                    }
                     break;
+                }
                 case PreferenceTopic.BySpecificIngredients:
-                    preferenceMethod = SortBySingleIngredient();
+                {
+                    Product p;
+                    int value;
+                    if (GetValuesFromUI(out p, out value, StackPanel_CurrentPreference))
+                    {
+                        preferenceMethod = SortBySingleIngredient(p, value);
+                    }
                     break;
+                }
                 case PreferenceTopic.ByIngredientsOwned:
-                    preferenceMethod = SortByIngredientsOwned();
+                {
+                    int value;
+                    GetValuesFromUI(out value, StackPanel_CurrentPreference, 1);
                     break;
+                }
                 case PreferenceTopic.ByExpirationDate:
+                {
                     preferenceMethod = SortByExpirationDate();
                     break;
+                }
                 default:
                     throw new ArgumentOutOfRangeException(nameof(choice), choice, null);
             }
-            throw new NotImplementedException();
+            return preferenceMethod;
+        }
+
+        private bool GetValuesFromUI(out int value, StackPanel stackPanel, int integerTextBoxIndex)
+        {
+            var itxt = stackPanel.Children[integerTextBoxIndex] as IntegerTextBox;
+            return int.TryParse(itxt.Text, out value);
+        }
+
+        private bool GetValuesFromUI<T>(out T selectedItem, out int value, StackPanel stackPanel) where T : class
+        {
+            var dp = stackPanel.Children[1] as DockPanelWithLabel;
+            var cbx = dp.SecondElement as ComboBox;
+            selectedItem = cbx.SelectedValue as T;
+            var itxt = StackPanel_CurrentPreference.Children[2] as IntegerTextBox;
+            return int.TryParse(itxt.Text, out value);   //what happens if its empty? Will it raise an exception regardless?
         }
 
         private Preference SortByExpirationDate()
         {
-            throw new NotImplementedException();
+            var method = new Action<ProgramData>(x => MessageBox.Show("No one knows"));
+            return new Preference(method);
         }
 
-        private Preference SortByIngredientsOwned()
+        private Preference SortByIngredientsOwned(int val)
         {
-            throw new NotImplementedException();
+            var method = new Action<ProgramData>(x => x.AllBoughtProducts.ForEach(y => y.Value += val));
+            return new Preference(method);
         }
 
-        private Preference SortBySingleIngredient()
+        private Preference SortBySingleIngredient(Product p, int val)
         {
-            throw new NotImplementedException();
+            var method = new Action<ProgramData>(x => x.AllProducts.Find(y => y.Equals(p)).Value += val);
+            return new Preference(method);
         }
 
-        private Preference SortByRecipeCategory()
+        private Preference SortByRecipeCategory(RecipeCategory rc, int val)
         {
-            throw new NotImplementedException();
+            var method = new Action<ProgramData>(x => x.AllRecipeCategories.Find(y => y.Equals(rc)).Value += val);
+            return new Preference(method);
         }
 
-        private Preference SortByProductCategory()
+        private Preference SortByProductCategory(ProductCategory pc, int val)
         {
-            throw new NotImplementedException();
+            var method = new Action<ProgramData>(x => x.AllProductCategories.Find(y => y.Equals(pc)).Value += val);
+            return new Preference(method);
         }
 
         private void AddPreference(Preference preference)
