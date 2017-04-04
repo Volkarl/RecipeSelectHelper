1f42e14d6c4a70d4bcc35a7ee7cca2f6248700a6
Karlzone <Karlzone0@gmail.com>
2017-04-04 14:21:30 +0200

    ExpirationDatePreference added, though untested and ExpirationInfo still has a possibly-incorrect formula.

--- RecipeSelectHelper/Model/ExpirationInfo.cs
+++ RecipeSelectHelper/Model/ExpirationInfo.cs
@@ -16,5 +16,17 @@ namespace RecipeSelectHelper.Model
             ProductCreatedTime = createdTime;
             ProductExpirationTime = expirationTime;
         }
+
+        public double GetExpiredPercentage(DateTime? timeNow = null)
+        {
+            if (ProductCreatedTime == null || ProductExpirationTime == null)
+            {
+                return 0;
+            }
+            double now = (timeNow ?? DateTime.Now).Ticks;
+            long diff = ProductExpirationTime.Value.Ticks - ProductCreatedTime.Value.Ticks;
+            return diff / now;
+        }
+        // Make sure the above one works and doesn't lose the fraction
     }
 }
--- RecipeSelectHelper/Model/SortingMethods/ExpirationDatePreference.cs
+++ RecipeSelectHelper/Model/SortingMethods/ExpirationDatePreference.cs
@@ -7,14 +7,27 @@ namespace RecipeSelectHelper.Model.SortingMethods
     [DataContract(Name = "ExpirationDatePreference")]
     public class ExpirationDatePreference : Preference
     {
+        [DataMember]
+        public int Val { get; set; }
+
+        private Func<double, double> _valueFormula = x => (-0.4371 * Math.Pow(x, 2)) + (15.851 * x) - 22.42;
+
         public ExpirationDatePreference(string description = null) : base(description)
         {
-            Description += nameof(ExpirationDatePreference) + " | No one knows what goes on here.";
+            Description += nameof(ExpirationDatePreference) + " | Finds expiration date value (between 0 and 120 depending on time to product expiration) and multiplies this by " + Val;
         }
 
         public override void Calculate(ProgramData pd)
         {
-            MessageBox.Show("No one knows");
+            DateTime now = DateTime.Now;
+            pd.AllBoughtProducts.ForEach(x => x.Value += CalculateValue(x.ExpirationData, now));
+        }
+
+        private int CalculateValue(ExpirationInfo exp, DateTime time)
+        {
+            int decimalExpired = (int)(exp.GetExpiredPercentage(time) * 100);
+            double formulaResult = _valueFormula(decimalExpired);
+            return Val * Convert.ToInt32(formulaResult);
         }
     }
 }
